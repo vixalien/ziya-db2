9c8b82309a18a415587747990f54f68c53f150d3
Angelo Shema <geoangercola@gmail.com>
2020-12-20 01:03:31 +0200

    In-memory again

--- api/index.js
+++ api/index.js
@@ -3,6 +3,7 @@ let express = require('express')();
 let PouchDB = require('pouchdb');
 let Server = require('express-pouchdb');
 
+PouchDB.plugin(require('pouchdb-adapter-memory'));
 express.use(Server(PouchDB.defaults({adapter: 'memory'})));
 
 express.listen(5984);
